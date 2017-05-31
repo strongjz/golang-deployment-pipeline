aaac129ae3fe70a9d43972ff3bb8e5a08da9b634
James Strong <strong.james.e@gmail.com>
2017-05-31 19:18:28 -0400

    testing the new version

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const version string = "3.0.0"
+const version string = "3.1.0"
 
 // VersionHandler handles incoming requests to /version
 // and just returns a simple version number
